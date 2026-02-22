c7083e474175af29940c111707363513eba95dab
wolfyc <zaidimedh@gmail.com>
2026-02-22 21:10:29 +0100

    final success code

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 test_settings = {
-    'theme':'dark',
-    'language':'enabled',
-    'notifications':'high'
+    'Theme':'dark',
+    'Notifications':'enabled',
+    'Volume':'high'
 }
-test_pair = ('Theme','white')
+
+def delete_setting(settings: dict, key: str):
+    key = key.lower()  
+    
+    if key in settings:  
+        settings.pop(key)  
+        return f"Setting '{key}' deleted successfully!" 
+    return "Setting not found!"
+
+def update_setting(settings:dict, pair:tuple):
+    key, value = pair
+    key=key.lower()
+    value = value.lower()
+
+    if key in settings:
+        settings[key] = value
+        return f"Setting '{key}' updated to '{value}' successfully!"
+    return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
 
 def add_setting(settings: dict, pair: tuple):
-    key = pair[0].lower()
-    value = pair[1].lower()
-    if key in settings:
-        return f'Setting {key} updated to {value} successfully!'
-        #print(f'Setting {key} already exists! Cannot add a new setting with this name.')
-    else:
-        settings.update({key:value})
-        return f'Setting {key} added with value {value}'
-    pass
+    key, value = pair
 
-print(add_setting({'theme': 'light'}, ('THEME', 'dark')))
+    key = key.lower()
+    value = value.lower()
+
+    if key in settings:
+        return f"Setting '{key}' already exists! Cannot add a new setting with this name."
+        
+    settings[key] = value
+    return f"Setting '{key}' added with value '{value}' successfully!"
+
+def view_settings(settings:dict):
+    if settings == {}:
+        return "No settings available."
+    
+    result = "Current User Settings:\n"
+    
+    
+    for key, value in settings.items():
+       
+        cap_key = key.capitalize()
+        result += f"{cap_key}: {value}\n"
+    
+    return result
+
+print(view_settings(test_settings)) 
